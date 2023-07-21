9f415e6d072e7eb789d96c508a05c65b6a02bb04
Mitsu32104 <mitsutoyo32104@gmail.com>
2023-07-21 12:43:49 +0900

    grid-layoutの微調整

---
+++ scripts/libs/text-animation.js
@@ -0,0 +1,66 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    new TextAnimation('.animate-title');
+});
+
+
+class TextAnimation {
+    constructor(els) {
+        this.DOM = {};
+        this.DOM.els = document.querySelectorAll(els);
+
+        // DOMの文字列を配列で保持
+        this.strings = this._pushString();
+
+        // 各文字列の各文字を分割して配列で保持
+        this.words = this._splitString();
+
+        // 各文字をspan.charで括る
+        this.chars = this._bindChar();
+
+        // 各文字列を.charで括った文字列に書き換える
+        this.chars.forEach(function(char, i) {
+            this.DOM.els[i].innerHTML = char;
+        }.bind(this));
+    }
+
+    _pushString() {
+        this.strings = [];
+
+        this.DOM.els.forEach(el => {
+            const string = el.textContent;
+            this.strings.push(string);
+        });
+
+        return this.strings;
+    }
+
+    _splitString() {
+        this.words = [];
+
+        this.strings.forEach(string => {
+            const word = string.split('');
+            this.words.push(word);
+        });
+
+        return this.words;
+    }
+
+    _bindChar() {
+        this.chars = [];
+
+        const _this = this;
+
+        this.words.forEach(function(chars) {
+            const words = chars.reduce((accu, curr) => {
+                return `${accu}<span class="char">${curr}</span>`;
+            }, '');
+
+            _this.chars.push(words);
+        });
+
+        return this.chars;
+    }
+}
+
+
