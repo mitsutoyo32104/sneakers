1503d48787bc8b4ced1fd1047f3ebdc8fac1d807
Mitsu32104 <mitsutoyo32104@gmail.com>
2023-08-21 10:53:47 +0900

    h1のSEO対策

--- scripts/libs/hero-slider.js
+++ scripts/libs/hero-slider.js
@@ -24,3 +24,39 @@ const swiper = new Swiper(".swiper", {
         el: ".swiper-scrollbar",
     },
 });
+
+class HeroSlider {
+    constructor(el, options) {
+        this.swiper = new Swiper(".swiper", options);
+        const options = _setDefaultOptions();
+    }
+
+    _setDefaultOptions() {
+        return {
+            // Optional parameters
+            direction: "horizontal",
+            loop: true,
+            effect: "coverflow",
+            speed: "600",
+            slidesPerView: 1.5,
+            spaceBetween: "0px",
+            centeredSlides: "true",
+
+            // If we need pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            // And if we need scrollbar
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        };
+    }
+}
